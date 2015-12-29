06df11589f1e7fa36a6ae3d20703e546d9f6fb45
ekremyurdakul <ekrem@yurdakul.net>
2015-12-29 21:59:13 +0200

    Authorisation and fixed some references on templates

--- trackstats/usrmgmt/urls.py
+++ trackstats/usrmgmt/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 app_name = 'usrmgmt'
 urlpatterns = [
-    url(r'^loginac/$', views.loginac, name='loginac'),
+    url(r'^login/$', views.login, name='auth'),
+    url(r'^loginac/$', views.auth, name='auth'),
     url(r'^logout/$', views.logout, {'next_page': '/',}, name='logout'),
 ]
--- trackstats/usrmgmt/views.py
+++ trackstats/usrmgmt/views.py
@@ -1,10 +1,81 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import QueryDict
+from django.views.decorators.csrf import csrf_exempt
+from httplib2 import Http
+import urllib
+import json
+import os
+import logging
+
+credentials_dir = os.path.join(os.path.dirname(__file__), '../ClientIDSecret.json')
+ACT_READ = "https://www.googleapis.com/auth/fitness.activity.read"
+LOC_READ = "https://www.googleapis.com/auth/fitness.location.read"
+BODY_READ = "https://www.googleapis.com/auth/fitness.body.read"
+with open(credentials_dir, mode = 'r') as cred:
+	data = json.load(cred)
+	CLIENT_ID = data["web"]["client_id"]
+	CLIENT_SECRET = data["web"]["client_secret"]
 
 # Create your views here.
+def login(request):
+    token_request_uri = "https://accounts.google.com/o/oauth2/auth"
+    response_type = "code"
+    client_id = CLIENT_ID
+    redirect_uri = "http://localhost:8080/usr/loginac/"
+    scope = ACT_READ+' '+LOC_READ+' '+BODY_READ
+    url = "{token_request_uri}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}".format(
+            token_request_uri=token_request_uri,
+            response_type=response_type,
+            client_id=client_id,
+            redirect_uri=redirect_uri,
+            scope=scope)
+    return HttpResponseRedirect(url)
 
-def loginac(request):
-    return HttpResponse("Reached the Authorization Code processing method")
+
+
+
+def auth(request):
+
+    parser = Http()
+    login_failed_url = '/'
+    authcode = request.GET['code']
+    logging.info(authcode)
+    if authcode:
+
+        access_token_uri = "https://www.googleapis.com/oauth2/v4/token"
+
+        params = urllib.urlencode({
+            'code': authcode,
+            'redirect_uri': "http://localhost:8080/usr/loginac/",
+            'client_id': "1047651366452-32ph4ndul3s5caub46t3nbue3p5e5uha",
+            'client_secret': "PNwEP-rCdeP8rBreZ8oWjUle",
+            'grant_type': 'authorization_code'
+        })
+
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+
+        resp, content = parser.request(access_token_uri, method = 'POST', body = params, headers = headers)
+        content = json.loads(content)
+        logging.info(type(content))
+        tokendata = content['access_token']
+
+        #request.session['ACCESSTOKEN'] = tokendata
+        # resp, content = parser.request("https://www.googleapis.com/oauth2/v1/userinfo?access_token={accessToken}".format(
+        #         accessToken=token_data['access_token']))
+        # this gets the google profile!!
+        # google_profile = json.load(content)
+
+        # log the user in-->
+        # HERE YOU LOG THE USER IN, OR ANYTHING ELSE YOU WANT
+        # THEN REDIRECT TO PROTECTED PAGE
+
+
+        return HttpResponseRedirect('/pages/dashboard')
+
+    else:
+        return HttpResponseRedirect('/pages/welcome')
 
 def logout(request):
-	return HttpResponse("Signed out!")
+    return HttpResponse("Signed out!")
